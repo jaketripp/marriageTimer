8b5746c3a798e0f8b9b9bbd83e65d029f6e13de9
Jake <shmogens@gmail.com>
2018-07-21 02:58:25 -0500

    Redo with momentjs to handle times better (months and years)

--- js/app.js
+++ js/app.js
@@ -1,61 +1,51 @@
-
-var lengthOfPomodoro = 25;
-var millisecondsPom = 1000 * 60 * lengthOfPomodoro;
-
-
-// ===================
-// AUXILIARY FUNCTIONS
-// ===================
-
 function pluralize(count, word) {
-    return count === 1 ? count + ' ' + word : count + ' ' + word + 's';
+  return count === 1 ? count + " " + word : count + " " + word + "s";
 }
 
-// converts hours to simplified time (1 year, 3 months, 2 weeks, 1 day, 9 hours) and appends string
-function formatTime(milliseconds) {
-    var value = milliseconds;
+function getTimeDuration() {
+  // July 20, 2017 11:00AM
+  var marriageDate = moment(1500566400000).locale("en");
+  // today plus one for some reason?
+  var now = moment()
+    .add(1, "day")
+    .locale("en");
+  var time = moment.duration(now.diff(marriageDate));
 
-    var units = {
-        "year": 24*365*60*60*1000,
-        "month": 24*30*60*60*1000,
-        "week": 24*7*60*60*1000,
-        "day": 24*60*60*1000,
-        "hour": 1*60*60*1000,
-        "minute": 1*60*1000,
-        "second": 1*1000
-    };
+  const { days, hours, minutes, months, seconds, years } = time._data;
 
-    var result = [];
-
-    for(var name in units) {
-        var p =  Math.floor(value/units[name]);
-        if (p > 0) {
-            result.push(pluralize(p,name));
-        }
-        value %= units[name];
-    }
-
-    var str = result[0];
-
-    for (var i = 1; i < result.length; i++) {
-        str += (', ' + result[i]);
-    }
-
-    return str;
+  return [
+    ["year", years],
+    ["month", months],
+    ["day", days],
+    ["hour", hours],
+    ["minute", minutes],
+    ["second", seconds]
+  ];
 }
 
-function updateDom(){
-	setTimeout(function(){
-        $('#loader').hide();
-    }, 1000);
-	setInterval(function(){
-		var marriageDate = new Date(2017, 6, 20, 11);
-		var currentDate = Date.now();
-		var seconds = (currentDate - marriageDate); 
-		var time = formatTime(seconds);
-		$('h2').text(time);
-	}, 1000);
+function buildDurationString(dataArr) {
+  return dataArr
+    .map(unitArr => {
+      if (unitArr[1] > 0) {
+        return pluralize(unitArr[1], unitArr[0]);
+      }
+    })
+    .filter(item => {
+      return !!item;
+    })
+    .join(", ");
+}
+
+function updateDom() {
+  setTimeout(function() {
+    $("#loader").hide();
+  }, 1000);
+
+  setInterval(function() {
+    let dataObj = getTimeDuration();
+    let result = buildDurationString(dataObj);
+    $("h2").text(result);
+  }, 1000);
 }
 
 updateDom();
-
